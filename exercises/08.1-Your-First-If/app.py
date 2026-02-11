2dde25c529fbeea78e73952576334a0cac990907
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:01:45 +0100

    My first if

--- exercises/08.1-Your-First-If/app.py
+++ exercises/08.1-Your-First-If/app.py
@@ -1,3 +1,11 @@
+# EJERCICIO 8.1 - your first if
 total = int(input('How much money do you have in your pocket\n'))
-
-# ✅ ↓ YOUR CODE HERE ↓ ✅
+# ↓ YOUR CODE HERE ↓
+if total > 100:
+    print("Give me your money!")
+else:
+    if total > 50:
+        print("Buy me some coffee, you cheap!")
+    else:
+        if total <= 50:
+            print("You are a poor guy, go away!")
