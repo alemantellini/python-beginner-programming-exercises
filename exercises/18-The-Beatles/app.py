f5dfd606ea94b25677f3c40b39cb92e7b7d60df2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:08:36 +0100

    The Beatles

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,13 @@
-# ✅↓ Write your code here ↓✅
-
+# EJERCICIO 18 - The Beatles
+def sing():
+    song = ""
+    for i in range(11):
+        if i == 4:
+            song += "there will be an answer,\n"
+        elif i == 10:
+            song += "whisper words of wisdom, let it be"
+        else:
+            song += "let it be,\n"
+    return song
+my_fav_song = sing()
+print(my_fav_song)
