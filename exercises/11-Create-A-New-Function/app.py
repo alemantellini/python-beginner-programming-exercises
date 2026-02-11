b4a524cc5344d5836e9020b5f3c6c00160acbcb9
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:04:03 +0100

    Creating a new function

--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,7 @@
+# EJERCICIO 11 - create a new function
 import random
-
-# ✅↓ Write your code here ↓✅
+# ↓ Write your code here ↓
+def generate_random():
+    return random.randint(0,9)
+result = generate_random()
+print(result)
