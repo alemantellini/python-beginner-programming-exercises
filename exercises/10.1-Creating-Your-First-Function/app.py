fe28fe0d6a4829a5f987181fa9b0723f17954599
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:03:42 +0100

    Creating my first function

--- exercises/10.1-Creating-Your-First-Function/app.py
+++ exercises/10.1-Creating-Your-First-Function/app.py
@@ -1,6 +1,6 @@
+# EJERCICIO 10.1 - creating your first function
 def add_numbers(a,b):
-	# This is the function's body ✅↓ Write your code here ↓✅
-	
-
-# ❌ ↓ DON'T CHANGE THE CODE BELOW ↓ ❌
+	# This is the function's body ↓ Write your code here ↓
+	return a + b
+# ↓ DON'T CHANGE THE CODE BELOW ↓
 print(add_numbers(3,4))
