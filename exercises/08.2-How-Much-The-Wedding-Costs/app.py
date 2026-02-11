822ca60c6dfb8f8a5ce5b4ac3cc7fb07624894e2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:02:19 +0100

    How much the wedding costs

--- exercises/08.2-How-Much-The-Wedding-Costs/app.py
+++ exercises/08.2-How-Much-The-Wedding-Costs/app.py
@@ -1,12 +1,14 @@
+# EJERCICIO 8.2 - how much the wedding costs
 user_input = int(input('How many people are coming to your wedding?\n'))
-
-# ❌ ⬆ DON'T CHANGE THE CODE ABOVE  ⬆ ❌
-
-
+# ⬆ DON'T CHANGE THE CODE ABOVE  ⬆
 if user_input <= 50:
     price = 4000
-# ✅ ↓ Your code here ↓ ✅
-
-
-# ❌ ↓ DON'T CHANGE THE CODE BELOW ↓ ❌
+# ↓ Your code here ↓
+elif user_input <= 100:
+    price = 10000
+elif user_input <= 200:
+    price = 15000
+elif user_input > 200:
+    price = 20000
+# ↓ DON'T CHANGE THE CODE BELOW ↓
 print('Your wedding will cost '+str(price)+' dollars')
