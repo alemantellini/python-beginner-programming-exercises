02eed7b5adb3d6430d4b3ff35da4e5c78cd3a442
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:06:57 +0100

    Russian Roulette

--- exercises/17-Russian-Roulette/app.py
+++ exercises/17-Russian-Roulette/app.py
@@ -1,15 +1,14 @@
+# EJERCICIO 17 - Russian Roulette
 import random
-
 bullet_position = 3
-
 def spin_chamber():
 	chamber_position = random.randint(1,6)
 	return chamber_position
-
-# ❌ ⬆ DON'T CHANGE THE CODE ABOVE ⬆ ❌
+# ⬆ DON'T CHANGE THE CODE ABOVE ⬆
 def fire_gun():
-	# ✅ ↓ your code here ↓ ✅
-	return None
-
-
+	# ↓ your code here ↓
+	if spin_chamber() == bullet_position:
+		return "You are dead!"
+	else:
+		return "Keep playing!"
 print(fire_gun())
