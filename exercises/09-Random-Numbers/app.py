aae0a7a50fe3f832f5ea8235fb8700a969d1fc1f
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:02:51 +0100

    Random numbers

--- exercises/09-Random-Numbers/app.py
+++ exercises/09-Random-Numbers/app.py
@@ -1,8 +1,7 @@
+# EJERCICIO 9 - random numbers
 import random
-
 def get_randomInt():
-  	# ✅ ↓ CHANGE ONLY THIS ONE LINE BELOW ↓ ✅
-	random_number = random.random()
+  	# ↓ CHANGE ONLY THIS ONE LINE BELOW ↓
+	random_number = random.randint(1, 10)
 	return random_number
-
 print(get_randomInt())
