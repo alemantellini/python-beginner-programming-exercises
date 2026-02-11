4673b333fbfc4ed2917aafc03802f6e1a71ef9c2
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-02-11 12:04:57 +0100

    Rand from 1 to 12

--- exercises/12-Rand-From-One-to-Twelve/app.py
+++ exercises/12-Rand-From-One-to-Twelve/app.py
@@ -1,8 +1,7 @@
+# EJERCICIO 12 - rand from one to twelve
 import random
-
 def get_randomInt():
-	# ✅↓ Write your code here ↓✅
-	return None
-
-# ❌ ↓ DON'T CHANGE THE CODE BELOW ↓ ❌
+	# ↓ Write your code here ↓
+	return random.randrange(1,13)
+# ↓ DON'T CHANGE THE CODE BELOW ↓
 print(get_randomInt())
